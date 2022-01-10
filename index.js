c08fe9f60a0a904dc38755cc5c2352564be2a067
nortzaya <76810120+nortzaya@users.noreply.github.com>
2022-01-10 18:58:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let interval = 0;
 let iteration = 0;
 client.on("ready", function () {
     console.log(`[SelfBot]: ${client.user?.username} Online`);
-    client.user?.setActivity({ name: "Cyberpunk 2077", type: "STREAMING", url: "https://twitch.tv/lisenok_257" });
+    client.user?.setActivity({ name: "God of War", type: "PLAYING", url: "https://twitch.tv/lisenok_257" });
 });
 let commands = new Map()
     .set("setAnimateStatus", function (message, parse) {
